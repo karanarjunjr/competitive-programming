6e43d784302382db9633a99ae3dea3916ecddaf9
Arjun J <69049250+karanarjunjr@users.noreply.github.com>
2022-03-31 18:50:59 +0530

    Modulo

--- Number Theory/Fast Exponentiation.cpp
+++ Number Theory/Fast Exponentiation.cpp
@@ -2,8 +2,8 @@ int power(int a, int b) {
     int ans = 1;
     while (b > 0) {
         if ((b & 1))
-            ans *= a;
-        a *= a;
+            ans = (ans * a) % mod;
+        a = (a * a) % mod;
         b = (b >> 1);
     }
     return ans;
